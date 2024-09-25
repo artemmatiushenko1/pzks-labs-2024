929e91da34901f60d62b4b75d55cf3917cb35376
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-09-25 20:32:36 +0300

    create token matcher

--- lab-1/expression-compiler/src/main/kotlin/LexicalAnalyzerImpl.kt
+++ lab-1/expression-compiler/src/main/kotlin/LexicalAnalyzerImpl.kt
@@ -1,66 +1,55 @@
 package org.example
 
+data class TokenMatcher(private val regex: Regex, val skip: Boolean = false, val tokenType: TokenType) {
+    fun match(input: String): String? {
+        return this.regex.find(input)?.value
+    }
+}
+
 class LexicalAnalyzerImpl(override val expressionSource: String) : LexicalAnalyzer {
     override val tokens: MutableList<Token> = mutableListOf()
 
-    override fun tokenize(): List<Token> {
-        val identifierRegex = Regex("^_*[a-zA-Z_0-9]+")
-        val mathOperatorRegex = Regex("^[+\\-/*]")
-        val whiteSpaceRegex = Regex("^\\s")
-        val openParenthesisRegex = Regex("^\\(")
-        val closeParenthesisRegex = Regex("^\\)")
-        val numberRegex = Regex("^([0-9]*[.])?[0-9]+")
+    private val matchers: List<TokenMatcher> = listOf(
+        TokenMatcher(regex = Regex("^\\s"), tokenType = TokenType.MATH_OPERATOR, skip = true),
+        TokenMatcher(regex = Regex("^\\("), tokenType = TokenType.OPEN_PAREN),
+        TokenMatcher(regex = Regex("^\\)"), tokenType = TokenType.CLOSE_PAREN),
+        TokenMatcher(regex = Regex("^([0-9]*[.])?[0-9]+"), tokenType = TokenType.NUMBER),
+        TokenMatcher(regex = Regex("^[+\\-/*]"), tokenType = TokenType.MATH_OPERATOR),
+        TokenMatcher(regex = Regex("^_*[a-zA-Z_0-9]+"), tokenType = TokenType.IDENTIFIER),
+    )
 
+    private fun findMatchedToken(input: String): Pair<TokenMatcher, Token>? {
+        for (matcher in this.matchers) {
+            val matchedTokenLexeme = matcher.match(input)
+
+            if (matchedTokenLexeme != null) {
+                return matcher to Token(type = matcher.tokenType, lexeme = matchedTokenLexeme)
+            }
+        }
+
+        return null
+    }
+
+    override fun tokenize(): List<Token> {
         var position = 0
 
         while (position < expressionSource.length) {
             val restOfExpression = expressionSource.slice(position until expressionSource.length)
 
-            val whiteSpaceMatch = whiteSpaceRegex.find(restOfExpression)
-            if (whiteSpaceMatch != null) {
+            val matchedTokenResult = this.findMatchedToken(restOfExpression)
+                ?: throw LexicalError("Unknown token '${expressionSource[position]}' at position $position.", position)
+
+            val (tokenMatcher, token) = matchedTokenResult
+
+            if (tokenMatcher.skip) {
                 position = position.inc()
                 continue
+            } else {
+                this.tokens.add(token)
+                position += token.lexeme.length
             }
-
-            val openParenthesisMatch = openParenthesisRegex.find(restOfExpression)
-            if (openParenthesisMatch != null) {
-                this.tokens.add(Token(type = TokenType.OPEN_PAREN, lexeme = openParenthesisMatch.value))
-                position += openParenthesisMatch.value.length
-                continue
-            }
-
-            val closeParenthesisMatch = closeParenthesisRegex.find(restOfExpression)
-            if (closeParenthesisMatch != null) {
-                this.tokens.add(Token(type = TokenType.CLOSE_PAREN, lexeme = closeParenthesisMatch.value))
-                position += closeParenthesisMatch.value.length
-                continue
-            }
-
-            val numberMatch = numberRegex.find(restOfExpression)
-            if (numberMatch != null) {
-                this.tokens.add(Token(type = TokenType.NUMBER, lexeme = numberMatch.value))
-                position += numberMatch.value.length
-                continue
-            }
-
-            val mathOperatorMatch = mathOperatorRegex.find(restOfExpression)
-            if (mathOperatorMatch != null) {
-                this.tokens.add(Token(type = TokenType.MATH_OPERATOR, lexeme = mathOperatorMatch.value))
-                position += mathOperatorMatch.value.length
-                continue
-            }
-
-            val identifierMatch = identifierRegex.find(restOfExpression)
-            if (identifierMatch != null) {
-                this.tokens.add(Token(type = TokenType.IDENTIFIER, lexeme = identifierMatch.value))
-                position += identifierMatch.value.length
-                continue
-            }
-
-            throw LexicalError("Unknown token '${expressionSource[position]}' at position $position.", position)
         }
 
         return this.tokens
     }
-
 }
