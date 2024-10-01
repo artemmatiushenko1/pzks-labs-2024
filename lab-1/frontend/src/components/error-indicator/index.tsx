8c20783b97ed08cdcfa31c7e8ca17c7e7cc1be86
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-10-01 22:01:30 +0300

    fix error indicator

--- lab-1/frontend/src/components/error-indicator/index.tsx
+++ lab-1/frontend/src/components/error-indicator/index.tsx
@@ -7,19 +7,22 @@ const ErrorIndicator = (props: ErrorIndicatorProps) => {
   const { expression, errorAtIndex } = props;
 
   return (
-    <div className="font-mono text-sm">
-      {expression.split('').map((token, index) => (
-        <div className="relative inline-block">
-          <span
-            className={index === errorAtIndex ? 'font-bold text-red-500' : ''}
-          >
-            {token}
+    <div className="font-mono text-sm bg-secondary px-2 border-l-2 rounded-r-md flex items-center text-nowrap overflow-x-auto overflow-y-hidden">
+      <div className="h-[45px] flex items-center">
+        {expression.split('').map((token, index) => (
+          <span className="relative inline-block">
+            <span
+              className={
+                index === errorAtIndex
+                  ? "font-bold text-red-500 before:content-['^'] before:-bottom-4 before:absolute before:text-md"
+                  : ''
+              }
+            >
+              {token}
+            </span>
           </span>
-          {errorAtIndex === index && (
-            <span className="absolute inset-x-0 -bottom-5 text-xl text-red-500 before:content-['^']"></span>
-          )}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
